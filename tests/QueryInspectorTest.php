8f3e10a1e20c6ed2a40a8e316ec898e1e0c52f8f
Jon Cram <webignition@gmail.com>
2021-10-29 11:27:39 +0100

    Import (#2)

---
+++ tests/QueryInspectorTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\Tests\DoctrineInspectors;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Statement;
+use Doctrine\ORM\EntityManagerInterface;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use SmartAssert\DoctrineInspectors\QueryInspector;
+
+class QueryInspectorTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * @dataProvider invokeDataProvider
+     *
+     * @param array<string, scalar> $queryParameters
+     */
+    public function testInvoke(string $query, array $queryParameters): void
+    {
+        $statement = \Mockery::mock(Statement::class);
+        $statement
+            ->shouldReceive('execute')
+            ->with($queryParameters)
+        ;
+
+        $connection = \Mockery::mock(Connection::class);
+        $connection
+            ->shouldReceive('prepare')
+            ->with($query)
+            ->andReturn($statement)
+        ;
+
+        $entityManager = \Mockery::mock(EntityManagerInterface::class);
+        $entityManager
+            ->shouldReceive('getConnection')
+            ->andReturn($connection)
+        ;
+
+        $inspector = new QueryInspector($entityManager, $query, $queryParameters);
+
+        ($inspector)();
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function invokeDataProvider(): array
+    {
+        return [
+            'query, no parameters' => [
+                'query' => 'SELECT 1',
+                'queryParameters' => [],
+            ],
+            'query, has parameters' => [
+                'query' => 'SELECT name FROM Entity WHERE id = :id',
+                'queryParameters' => [
+                    'id' => 123,
+                ],
+            ],
+        ];
+    }
+}
